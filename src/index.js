ef254b0004a17f25b8cc87965ad0836e6272143c
rewnd <rewnd@protonmail.com>
2018-05-15 18:43:27 +0300

    Create empty game field

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+const canvas = document.getElementById('main-canvas');
+const ctx = canvas.getContext('2d');
 
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+let framerate = 100;
+
+class GameFunctions {
+  static drawBG() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  }
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  GameFunctions.drawBG();
+
+  setTimeout(gameLoop, framerate);
+}
+
+gameLoop();
