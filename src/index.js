32c8c16c4738c847b3cd255fa06839189dbaea2e
rewnd <rewnd@protonmail.com>
2018-05-15 20:01:57 +0300

    Add Snake class

--- src/index.js
+++ src/index.js
@@ -52,6 +52,67 @@ class Wall {
   }
 }
 
+class Snake {
+  constructor(startPosX, startPosY, length, snakeColor, snakeStrokeColor) {
+    this.segments = [];
+
+    for (let i = length; i > 0; i--) {
+      this.segments.push(new Block(startPosX + i, startPosY, snakeColor, snakeStrokeColor));
+    }
+
+    this.color = snakeColor;
+    this.direction = 'right';
+    this.nextDirection = 'right';
+  }
+
+  draw() {
+    this.segments.forEach(elem => elem.draw());
+  }
+
+  setDirection(newDirection) {
+    if (newDirection === 'left' && this.direction === 'right') {
+      return;
+    } else if (newDirection === 'right' && this.direction === 'left') {
+      return;
+    } else if (newDirection === 'up' && this.direction === 'down') {
+      return;
+    } else if (newDirection === 'down' && this.direction === 'up') {
+      return;
+    }
+
+    this.nextDirection = newDirection;
+  }
+
+  move() {
+    const snakeHead = this.segments[0];
+    let newHead;
+
+    this.direction = this.nextDirection;
+
+    switch (this.direction) {
+      case 'left':
+        newHead = new Block(snakeHead.posX - 1, snakeHead.posY, this.color);
+        break;
+      case 'right':
+        newHead = new Block(snakeHead.posX + 1, snakeHead.posY, this.color);
+        break;
+      case 'up':
+        newHead = new Block(snakeHead.posX, snakeHead.posY - 1, this.color);
+        break;
+      case 'down':
+        newHead = new Block(snakeHead.posX, snakeHead.posY + 1, this.color);
+        break;
+      default:
+        break;
+    }
+
+    this.segments.unshift(newHead);
+
+    // here will be apple eating code
+    this.segments.pop();
+  }
+}
+
 class GameFunctions {
   static drawBG() {
     ctx.fillStyle = 'black';
@@ -67,12 +128,33 @@ class GameFunctions {
   }
 }
 
+const snake = new Snake(5, 5, 3, 'darkgreen');
+
 function gameLoop() {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   GameFunctions.drawBG();
+  snake.move();
+  snake.draw();
   GameFunctions.drawBorderWalls('red');
-
   setTimeout(gameLoop, framerate);
 }
 
 gameLoop();
+
+// controls
+const directionCodes = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+};
+
+function handleKeys(e) {
+  const newDirection = directionCodes[e.keyCode];
+
+  if (newDirection !== undefined) {
+    snake.setDirection(newDirection);
+  }
+}
+
+window.addEventListener('keydown', handleKeys, true);
